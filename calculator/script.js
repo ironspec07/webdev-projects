dc66d0c089e1fc9b88ae3be51ebc01634710f03e
ironspec <ironspec07@gmail.com>
2023-05-21 15:47:08 +0530

    initial commit

---
+++ calculator/script.js
@@ -0,0 +1,19 @@
+let result = document.getElementById("result");
+
+function insert(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = "";
+}
+
+function backspace() {
+  result.value = result.value.slice(0, -1);
+}
+
+function calculate() {
+  let expression = result.value;
+  let answer = eval(expression);
+  result.value = answer;
+}
---
+++ color flipper/script.js
@@ -0,0 +1,14 @@
+const colors = ["red","green","blue","grey","rgba(133,122,200)","#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+ //get a random number between 0-5 
+ const randomNumber = getRandomNumber();
+ document.body.style.backgroundColor = colors[randomNumber];
+ color.textContent =  colors[randomNumber];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
